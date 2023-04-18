ac59b320691820e942ef73ffcfdf6102d9599b93
Muhammad Raihan Widagdo <88309114+widzzz@users.noreply.github.com>
2023-04-18 21:46:47 +0700

    Add JSON error handler

---
+++ server/src/handleParsingError.ts
@@ -0,0 +1,11 @@
+import { Request, Response, NextFunction } from "express";
+
+function handleParsingError(err: any, req: Request, res: Response, next: NextFunction) {
+    if (err instanceof SyntaxError) {
+        res.status(400).json({ message: 'Invalid JSON format' });
+    } else {
+        next();
+    }
+}
+
+export default handleParsingError;
