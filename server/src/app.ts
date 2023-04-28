29a8c86d8e2cdf4bdab6993e67c3758b942b65da
Muhammad Raihan Widagdo <88309114+widzzz@users.noreply.github.com>
2023-04-28 22:03:58 +0700

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -9,7 +9,7 @@ import handleParsingError from "./middlewares/handleParsingError";
 
 // Import user route
 import { registerUserHandler, loginUserHandler, createOrderUserHandler, getAllOrdersUserHandler, getOrderDetailUserHandler, updateOrderDetailUserHandler, deleteOrderDetailUserHandler } from "./routes/userRoute";
-import { createOrderHistoryAdminHandler, getAllOrdersAdminHandler, loginAdminHandler, registerAdminHandler, searchListUsernameHandler, updateOrderDetailsAdminHandler, updateTechnicianAdminHandler } from "./routes/adminRoute";
+import { createOrderHistoryAdminHandler, createStoreAdminHandler, getAllOrdersAdminHandler, getAllTechnicianDetails, loginAdminHandler, registerAdminHandler, registerTechnicianAdminHandler, searchListUsernameHandler, updateOrderDetailsAdminHandler, updateStoreAdminHandler, updateTechnicianAdminHandler } from "./routes/adminRoute";
 import { rejectEmptyStringBody, rejectEmptyStringParams } from "./middlewares/rejectEmptyString";
 import { usernameValidator } from "./middlewares/usernameValidator";
 
@@ -30,30 +30,53 @@ app.use(cors());
 app.use(helmet());
 app.use(handleParsingError);
 
-// Normal user router
+// NORMAL USER
 app.post('/api/register', rejectEmptyStringBody, usernameValidator, registerUserHandler);
 app.post('/api/login', rejectEmptyStringBody, loginUserHandler);
-app.post('/api/orders', authenticateToken, createOrderUserHandler);
+// ORDER ROUTE
+// Create order
+app.post('/api/orders/:username', authenticateToken, createOrderUserHandler);
+// Get all order of specific username
 app.get('/api/orders/:username', authenticateToken, rejectEmptyStringParams, getAllOrdersUserHandler);
+// Get order detail
 app.get('/api/orders/:username/:orders_id', authenticateToken, rejectEmptyStringParams, getOrderDetailUserHandler);
+// Update user order
 app.put('/api/orders/:username/:orders_id', authenticateToken, rejectEmptyStringParams, rejectEmptyStringBody, updateOrderDetailUserHandler);
+// Delete order
 app.delete('/api/orders/:username/:orders_id', authenticateAdminToken, rejectEmptyStringParams, deleteOrderDetailUserHandler);
 
-// Admin user router
+// ADMIN USER
+// Register new admin
 app.post('/api/admin/register', rejectEmptyStringBody, usernameValidator, registerAdminHandler);
+// Login admin
 app.post('/api/admin/login', rejectEmptyStringBody, loginAdminHandler);
+// ORDER ROUTE
+// Get all order list
 app.get('/api/admin/orders', authenticateAdminToken, getAllOrdersAdminHandler);
-app.get('/api/admin/search/username/:username', authenticateAdminToken, searchListUsernameHandler);
-app.get('/api/admin/technician', authenticateAdminToken);
-app.post('/api/admin/technician', authenticateAdminToken, rejectEmptyStringBody, registerAdminHandler);
-app.get('/api/admin/technician', authenticateAdminToken, getAllOrdersAdminHandler)
-app.put('/api/admin/technician/:id', authenticateAdminToken, rejectEmptyStringParams, rejectEmptyStringBody, updateTechnicianAdminHandler);
-app.put('/api/admin/orders/:username/:orders_id', authenticateAdminToken, rejectEmptyStringParams, rejectEmptyStringBody, updateOrderDetailsAdminHandler);
-app.post('/api/admin/orders/history', authenticateAdminToken, createOrderHistoryAdminHandler)
-app.post('api/admin/store')
+// Update order details
+app.put('/api/admin/orders/:username/:order_id', authenticateAdminToken, rejectEmptyStringParams, rejectEmptyStringBody, updateOrderDetailsAdminHandler);
+// ORDER HISTORY ROUTE
+// Create order history
+app.post('/api/admin/orders/:username/:order_id/history', authenticateAdminToken, createOrderHistoryAdminHandler)
+// TECHNICIAN ROUTE
+// Get all technician details
+app.get('/api/admin/technician', authenticateAdminToken, getAllTechnicianDetails);
+// Register technician
+app.post('/api/admin/technician', authenticateAdminToken, rejectEmptyStringBody, registerTechnicianAdminHandler);
+// Get all technician list and details
+app.get('/api/admin/technician', authenticateAdminToken, getAllTechnicianDetails);
+// Update specific technician details
+app.put('/api/admin/technician/:technician_id', authenticateAdminToken, rejectEmptyStringParams, rejectEmptyStringBody, updateTechnicianAdminHandler);
+// STORE ROUTE
+// Create store
+app.post('/api/admin/store', authenticateAdminToken, rejectEmptyStringBody, createStoreAdminHandler);
+// Update Store
+app.put('/api/admin/store/:store_id', authenticateAdminToken, rejectEmptyStringBody, updateStoreAdminHandler);
 // OTW
 // app.delete('/api/admin/technician/:id')
 // app.delete('/api/admin/orders/:username/:orders_id')
+// Seems weird implementation of search, possible remove or modify
+app.get('/api/admin/search/username/:username', authenticateAdminToken, searchListUsernameHandler);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
