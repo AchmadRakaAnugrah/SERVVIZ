980cca68887645fe891d6cd6b009ded2ecd459aa
Muhammad Raihan Widagdo <88309114+widzzz@users.noreply.github.com>
2023-05-22 22:55:51 +0700

    Add authStore (token and auth state)

--- client/src/main.ts
+++ client/src/main.ts
@@ -2,13 +2,28 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import axios from 'axios';
 
 import App from './App.vue'
 import router from './router'
+import { useAuthStore } from './store';
 
 const app = createApp(App)
 
-app.use(createPinia())
+const pinia = createPinia();
+
+app.use(pinia)
 app.use(router)
 
 app.mount('#app')
+
+axios.defaults.baseURL = 'http://localhost:5000';
+
+axios.interceptors.request.use((config) => {
+    const authStore = useAuthStore(); // Get the auth store
+    const token = authStore.token;
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  });
