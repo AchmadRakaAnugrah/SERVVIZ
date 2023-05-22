6ae9d3c8e7c9b322c59dd2a540577ce8494ef783
Muhammad Raihan Widagdo <88309114+widzzz@users.noreply.github.com>
2023-05-22 22:57:02 +0700

    Add sign in capability (alpha implement)

--- client/src/components/Navbar.vue
+++ client/src/components/Navbar.vue
@@ -1,35 +1,27 @@
-<script setup lang="ts">
-import 'bootstrap/dist/css/bootstrap.css';
-import { RouterLink } from 'vue-router';
-
-</script>
-
 <template>
-<!-- Navbar -->
-<nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <!-- Container wrapper -->
-  <div class="container-fluid">
-    <!-- Toggle button -->
-    <button class="navbar-toggler" type="button" data-mdb-toggle="collapse"
-      data-mdb-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-      aria-expanded="false" aria-label="Toggle navigation">
-      <i class="fas fa-bars"></i>
-    </button>
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <!-- Container wrapper -->
+    <div class="container-fluid">
+      <!-- Toggle button -->
+      <button class="navbar-toggler" type="button" data-mdb-toggle="collapse" data-mdb-target="#navbarSupportedContent"
+        aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <i class="fas fa-bars"></i>
+      </button>
 
-    <!-- Collapsible wrapper -->
-    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-      <!-- Navbar brand -->
-      <a class="navbar-brand mt-2 mt-lg-0" href="#">
-        <img src="../../public/deviantart.png" height="30"
-          alt="MDB Logo" loading="lazy" />
-      </a>
-      <!-- Left links -->
+      <!-- Collapsible wrapper -->
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <!-- Navbar brand -->
+        <a class="navbar-brand mt-2 mt-lg-0" href="#">
+          <img src="../../public/deviantart.png" height="30" alt="MDB Logo" loading="lazy" />
+        </a>
+        <!-- Left links -->
 
-    </div>
-    <!-- Collapsible wrapper -->
+      </div>
+      <!-- Collapsible wrapper -->
 
-    <!-- Right elements -->
-    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+      <!-- Right elements -->
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
         <li class="nav-item">
           <router-link to="/">
             <a class="nav-link">Home</a>
@@ -52,16 +44,25 @@ import { RouterLink } from 'vue-router';
         </li>
       </ul>
       <!-- Left links -->
-    <div class="d-flex align-items-center">
-      <router-link to="/SignInView.vue">
-        <button type="button" class="btn btn-primary me-3">
-          Sign In
-        </button>
-      </router-link>
+      <div class="d-flex align-items-center">
+        <router-link to="/SignInView.vue">
+          <button type="button" class="btn btn-primary me-3" v-if="!isAuthenticated">
+            Sign In
+          </button>
+        </router-link>
+      </div>
+      <!-- Right elements -->
     </div>
-    <!-- Right elements -->
-  </div>
-  <!-- Container wrapper -->
-</nav>
-<!-- Navbar -->
+    <!-- Container wrapper -->
+  </nav>
+  <!-- Navbar -->
 </template>
+
+<script setup lang="ts">
+  import { useAuthStore } from '@/store';
+  import 'bootstrap/dist/css/bootstrap.css';
+  import { RouterLink } from 'vue-router';
+
+  const authStore = useAuthStore();
+  const isAuthenticated = authStore.isAuthenticated;
+</script>
--- client/src/views/SignInView.vue
+++ client/src/views/SignInView.vue
@@ -12,8 +12,8 @@ import 'bootstrap';
             <h5 class="card-title">Sign In</h5>
             <form @submit.prevent="signIn">
               <div class="form-group">
-                <label for="email">Email address</label>
-                <input type="email" class="form-control" id="email" v-model="email" required>
+                <label for="username">Username</label>
+                <input type="username" class="form-control" id="username" v-model="username" required>
               </div>
               <div class="form-group">
                 <label for="password">Password</label>
@@ -30,23 +30,40 @@ import 'bootstrap';
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import axios, { AxiosError } from 'axios';
+import { useAuthStore } from '@/store';
 
 export default defineComponent({
   data() {
     return {
-      email: '',
-      password: ''
+      username: '',
+      password: '',
+      authenticated: false,
     };
   },
   methods: {
-    signIn() {
-      // Perform sign-in logic
-      console.log('Email:', this.email);
-      console.log('Password:', this.password);
+    async signIn() {
+      try {
+        const response = await axios.post('/api/login', {
+          username: this.username,
+          password: this.password,
+        });
 
-      // Reset form fields
-      this.email = '';
-      this.password = '';
+        const token = response.data.token;
+        const authStore = useAuthStore();
+        authStore.setToken(token);
+
+        
+        this.$router.push('/');
+      }
+      catch (error) {
+        if ((error as any).response && (error as any).response.status === 401) {
+          console.log(error)
+        } else {
+          // Handle other types of errors
+          // ...
+        }
+      }
     }
   }
 });
@@ -59,9 +76,11 @@ export default defineComponent({
   justify-content: center;
   align-items: center;
 }
+
 .form-group {
   margin-bottom: 20px;
 }
+
 .card {
   width: 100%;
   max-width: 400px;
