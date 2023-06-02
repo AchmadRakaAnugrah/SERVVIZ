c6df4c0db53c469538bfbbc781ec2752e52ee45b
dylansanjaya <dylanhikmasanjaya@gmail.com>
2023-06-02 13:44:24 +0700

    Revert "Add vuetify but still broken"
    This reverts commit 2d5eb5807f0e632c4acac727f47dbeca7a888fac.

--- client/src/main.js
+++ client/src/main.js
@@ -9,22 +9,10 @@ import "./assets/css/nucleo-svg.css";
 
 import materialKit from "./material-kit";
 
-// Vuetify
-import 'vuetify/styles'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-
-const vuetify = createVuetify({
-  components,
-  directives,
-})
-
 const app = createApp(App);
 const pinia = createPinia();
 
 app.use(pinia);
-app.use(vuetify);
 app.use(router);
 app.use(materialKit);
 app.mount("#app");
