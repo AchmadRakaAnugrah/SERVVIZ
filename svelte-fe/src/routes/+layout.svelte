51ac54a9c9a4fc42bc709af4476912a9219c15fb
Muhammad Raihan Widagdo <88309114+widzzz@users.noreply.github.com>
2023-06-08 20:24:33 +0700

    Svelte here

---
+++ svelte-fe/src/routes/+layout.svelte
@@ -0,0 +1,38 @@
+<script>
+  import "../app.postcss";
+  import {
+    Navbar,
+    NavBrand,
+    NavLi,
+    NavUl,
+    NavHamburger,
+    Button,
+    Input,
+  } from "flowbite-svelte";
+</script>
+
+<Navbar let:hidden let:toggle>
+  <NavBrand href="/">
+    <!-- <img
+      src="/images/flowbite-svelte-icon-logo.svg"
+      class="mr-3 h-6 sm:h-9"
+      alt="Flowbite Logo"
+    /> -->
+    <span
+      class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+    >
+      SER<b>VVIZ</b>
+    </span>
+  </NavBrand>
+  <div class="flex md:order-2">
+    <Button size="sm" href="/login">Log In</Button>
+    <NavHamburger on:click={toggle} />
+  </div>
+  <NavUl {hidden} class="order-1">
+    <NavLi href="/">Home</NavLi>
+    <NavLi href="/about">About</NavLi>
+    <NavLi href="/services">Track Order</NavLi>
+  </NavUl>
+</Navbar>
+
+<slot />
