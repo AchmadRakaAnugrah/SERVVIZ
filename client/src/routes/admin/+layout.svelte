fc4dde6d8834bff8a5de62eb3a64d42819ea565a
Muhammad Raihan Widagdo <88309114+widzzz@users.noreply.github.com>
2023-06-12 19:00:25 +0700

    Edit layout on admin

--- client/src/routes/admin/+layout.svelte
+++ client/src/routes/admin/+layout.svelte
@@ -3,9 +3,12 @@
   import {
     Navbar,
     NavBrand,
+    NavLi,
+    NavUl,
     NavHamburger,
     Button,
   } from "flowbite-svelte";
+  import { Footer, FooterCopyright, FooterLinkGroup } from "flowbite-svelte"
   import { jwtToken } from "../store";
 
   function handleLogout() {
@@ -16,18 +19,18 @@
 </script>
 
 <Navbar let:hidden let:toggle>
-  <NavBrand>
+  <NavBrand href="/home">
     <span
       class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
     >
-      SER<b>VVIZ</b>/admin
+      SER<b>VVIZ</b> / admin
     </span>
   </NavBrand>
   <div class="flex md:order-2">
     {#if $jwtToken === ''}
-      <!-- <Button size="sm" href="/signin">Sign In</Button> -->
+      <Button size="sm" href="/admin/signin">Sign In</Button>
     {:else}
-      <Button size="sm" href="/admin" on:click={handleLogout}>Sign Out</Button>
+      <Button size="sm" href="/" on:click={handleLogout}>Sign Out</Button>
     {/if}
     <NavHamburger on:click={toggle} />
   </div>
@@ -35,3 +38,14 @@
 
 <slot />
 
+<Footer>
+
+  <FooterCopyright href="/home" by="Flowbite™" year={2023} />
+  <FooterLinkGroup ulClass="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
+    <span
+    class="self-center whitespace-nowrap text-xl font-semibold dark:text-white"
+  >
+    SER<b>VVIZ</b> / admin
+  </span>
+  </FooterLinkGroup>
+</Footer>
