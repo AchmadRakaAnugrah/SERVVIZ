0f1c32bf8be2ab3c5cd32f6a4d5cc3751a5e5ecd
Muhammad Raihan Widagdo <88309114+widzzz@users.noreply.github.com>
2023-06-13 00:08:38 +0700

    Revert "njir"
    This reverts commit 9c0fba441e94dc040f40abc1b7d72109f79946f2, reversing
    changes made to 5a234f5e526eb4ab0e9b410ec10b15a9ace1ad0c.

--- client/src/routes/(user)/team/+page.svelte
+++ client/src/routes/(user)/team/+page.svelte
@@ -16,31 +16,31 @@
         alt: 'Raka',
         name: 'Raka',
         jobTitle: 'Front-end',
-        description: 'responsible for creating an attractive and easy-to-use interface, and ensuring that the app runs well.'
+        description: 'responsible for creating an attractive and easy-to-use interface, and ensuring that the app runs well on a variety of different devices and browsers.'
       },
       {
         href: '/',
         src: 'https://i.pinimg.com/236x/f9/9b/91/f99b910443c7c4c15a4024d1709c2b6a.jpg',
         alt: 'Raffi',
         name: 'Raffi',
-        jobTitle: 'UI/UX',
-        description: 'Responsible for a comprehennsive user experiences, intuitive interfaces, and visually stunning designs.'
+        jobTitle: 'DATABASE',
+        description: 'Responsible for designing, building, and maintaining an efficient and accessible database structure. Database concepts such as relational models, SQL queries, and database normalization.'
       },
       {
         href: '/',
         src: 'https://i.pinimg.com/236x/a6/bd/0b/a6bd0bb143610f3b574a29dba7933590.jpg',
         alt: 'Dylan',
         name: 'Dylan',
-        jobTitle: 'Project Manager - WebDeveloper',
-        description: 'Designed and oversought the application development, mainly front-end, database, application flow design'
+        jobTitle: 'Backend',
+        description: 'I am responsible for developing applications or websites that focus on managing servers, databases and APIs and everything'
       },
       {
         href: '/',
         src: 'https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/sofia-mcguire.png',
         alt: 'Raihan',
         name: 'Raihan',
-        jobTitle: 'Backend Engineer',
-        description: 'responsible for developing applications or websites that focus on managing servers, databases and APIs and everything.'
+        jobTitle: 'Backend',
+        description: 'I am responsible for developing applications or websites that focus on managing servers, databases and APIs and everything.'
       }
     ];
   </script>
@@ -49,13 +49,18 @@
     <TeamHeader>
       <svelte:fragment slot="label">Our Team</svelte:fragment>
       <p class="font-light text-gray-500 lg:mb-16 sm:text-xl dark:text-gray-400">
-       Meet our team who made all of this come to life
+       Jobd Desk
       </p>
     </TeamHeader>
     <TeamBody>
       {#each members as { href, src, alt, name, jobTitle, description }}
       <TeamItem {href} {src} {alt} {name} {jobTitle}>
         <p class="mt-3 mb-4 font-light text-gray-500 dark:text-gray-400">{description}</p>
+        <svelte:fragment slot="social">
+          <Facebook href="/" />
+          <Github href="/" />
+          <Twitter href="/" />
+        </svelte:fragment>
       </TeamItem>
       {/each}
     </TeamBody>
