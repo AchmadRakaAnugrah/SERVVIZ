f4d822e371181e724538328023068d1c7360f178
dylansanjaya <dylanhikmasanjaya@gmail.com>
2023-06-12 16:26:54 +0700

    Revert "tables in admin dashboard"
    This reverts commit 9c9627f52e7a01580cf0430644910ca7df8cf0e9.

--- client/src/routes/admin/(dashboard)/dashboard/+page.svelte
+++ client/src/routes/admin/(dashboard)/dashboard/+page.svelte
@@ -14,9 +14,8 @@
   import { Tabs, TabItem } from "flowbite-svelte";
   //fetch dari database
   export let Orders = [{ status: "Done" }];
-  export let History = [{ status: "Done" }];
   export let Technicians = [{ status: "Done" }];
-  export let Stores = [{ status: "Done" }];
+  export let Stores = [];
 
   //CRUD Modal
   import {
@@ -29,10 +28,7 @@
   } from "flowbite-svelte";
   import { onMount } from "svelte";
 
-  let clickOutsideModal1 = false;
-  let clickOutsideModal2 = false;
-  let clickOutsideModal3 = false;
-  let clickOutsideModal4 = false;
+  let clickOutsideModal = false;
 
   // Orders
   async function loadOrders() {
@@ -55,27 +51,6 @@
     itemsOrders = result.dataOrders;
   });
 
-  // Order History
-  async function loadHistory() {
-    const response = await fetch("http://localhost:5000/api/admin/orders/:username/:order_id/history", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFkbWluMSIsImlhdCI6MTY4NjUwNzMyOCwiZXhwIjoxNjg2NTEwOTI4fQ.8caIIxKNzULCYlJnbZojKQKbVvRIx11X03woBOcMX_4",
-      },
-    });
-    const dataHistory = await response.json();
-    return { dataHistory };
-  }
-
-  let itemsHistory = [];
-  onMount(async () => {
-    const result = await loadHistory();
-    console.log(result);
-    itemsHistory = result.dataHistory;
-  });
-
   //Technician
   async function loadTechnicians() {
     const response = await fetch("http://localhost:5000/api/admin/technician", {
@@ -83,7 +58,7 @@
       headers: {
         "Content-Type": "application/json",
         Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFkbWluMSIsImlhdCI6MTY4NjU0NTgwOCwiZXhwIjoxNjg2NTQ5NDA4fQ.GLRQiyOoStud47NsWMqFYyiCXdkx18WmxunFGrG5ezw",
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFkbWluMSIsImlhdCI6MTY4NjUwNzMyOCwiZXhwIjoxNjg2NTEwOTI4fQ.8caIIxKNzULCYlJnbZojKQKbVvRIx11X03woBOcMX_4",
       },
     });
     const dataTechnicians = await response.json();
@@ -95,27 +70,6 @@
     const result = await loadTechnicians();
     console.log(result);
     itemsTechnicians = result.dataTechnicians;
-  });
-
-  //Stores
-  async function loadStores() {
-    const response = await fetch("http://localhost:5000/api/admin/store", {
-      method: "GET",
-      headers: {
-        "Content-Type": "application/json",
-        Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImFkbWluMSIsImlhdCI6MTY4NjU0NTgwOCwiZXhwIjoxNjg2NTQ5NDA4fQ.GLRQiyOoStud47NsWMqFYyiCXdkx18WmxunFGrG5ezw",
-      },
-    });
-    const dataStores = await response.json();
-    return { dataStores };
-  }
-
-  let itemsStores = [];
-  onMount(async () => {
-    const result = await loadStores();
-    console.log(result);
-    itemsStores = result.dataStores;
   });
 
   const handleUpdate = () => {
@@ -134,7 +88,6 @@
     {value:"Processing", name: "Processing"},
     {value:"Haulted", name: "Haulted"},
     {value:"Waiting for payment", name: "Waiting for payment"},
-    {value:"Canceled", name: "Canceled"},
     {value:"Done", name: "Done"},
   ]
 
@@ -171,13 +124,13 @@
                 </TableBodyCell>
                 <TableBodyCell>
                   <div class="flex justify-left">
-                    <Button on:click={() => (clickOutsideModal1 = true)}
+                    <Button on:click={() => (clickOutsideModal = true)}
                       >Update</Button
                     >
                   </div>
                   <Modal
                     title="Update order ID: {item.id}"
-                    bind:open={clickOutsideModal1}
+                    bind:open={clickOutsideModal}
                     class="min-w-full"
                     autoclose 
                     outsideclose
@@ -231,83 +184,6 @@
           </TableBody>
         </Table>
       </TabItem>
-      <TabItem open title="Order History">
-        <Table>
-          <TableHead>
-            <TableHeadCell class="!p-1" />
-            <TableHeadCell>Actions</TableHeadCell>
-            <TableHeadCell>ID</TableHeadCell>
-            <TableHeadCell>Order ID</TableHeadCell>
-            <TableHeadCell>Technician ID</TableHeadCell>
-            <TableHeadCell>Technician ID</TableHeadCell>
-            <TableHeadCell>Datetime</TableHeadCell>
-            <TableHeadCell>Order Status</TableHeadCell>
-            <TableHeadCell>Description</TableHeadCell>
-          </TableHead>
-          <TableBody>
-            {#each itemsHistory as item}
-              <TableBodyRow>
-                <TableBodyCell class="!p-1">
-                  <!-- <Checkbox /> -->
-                </TableBodyCell>
-                <TableBodyCell>
-                  <div class="flex justify-left">
-                    <Button on:click={() => (clickOutsideModal2 = true)}
-                      >Update</Button
-                    >
-                  </div>
-                  <Modal
-                    title="Update order ID: {item.id}"
-                    bind:open={clickOutsideModal2}
-                    class="min-w-full"
-                    autoclose 
-                    outsideclose
-                  >
-                  <form on:submit={handleSubmit}>
-                    
-                    <div class="grid gap-4 mb-4 sm:grid-cols-2">
-                      <div>
-                        <Label>Technician
-                          <Select class="mt-2" items={technician_id} bind:value={item.technician_id} />
-                        </Label>
-                      </div>
-                      <div>
-                    <div class="flex items-center space-x-4">
-                      <div class="sm:col-span-2">
-                        <Label for="description" class="mb-2">Description</Label>
-                        <Textarea id="description" placeholder="{item.description}" rows="4" name="description"/>
-                      </div>
-                      
-                      <Button
-                        type="submit"
-                        class="w-fit"
-                        on:click={handleUpdate}
-                      >
-                        Update product
-                      </Button>
-                      <Button
-                        type="submit"
-                        class="w-fit"
-                        outline
-                        color="red"
-                        on:click={handleDelete}
-                      >
-                        Delete
-                      </Button>
-                    </div>
-                  </Modal>
-                </TableBodyCell>
-                <TableBodyCell>{item.id}</TableBodyCell>
-                <TableBodyCell>{item.order_id}</TableBodyCell>
-                <TableBodyCell>{item.technician_id}</TableBodyCell>
-                <TableBodyCell>{item.datetime}</TableBodyCell>
-                <TableBodyCell>{item.status}</TableBodyCell>
-                <TableBodyCell>{item.description}</TableBodyCell>
-              </TableBodyRow>
-            {/each}
-          </TableBody>
-        </Table>
-      </TabItem>
       <TabItem title="Technicians">
         <Table>
           <TableHead>
@@ -325,13 +201,13 @@
                 </TableBodyCell>
                 <TableBodyCell>
                   <div class="flex justify-left">
-                    <Button on:click={() => (clickOutsideModal3 = true)}
+                    <Button on:click={() => (clickOutsideModal = true)}
                       >Update</Button
                     >
                   </div>
                   <Modal
                     title="Update {item.name} Information"
-                    bind:open={clickOutsideModal3}
+                    bind:open={clickOutsideModal}
                     autoclose
                     outsideclose
                     class="min-w-full"
@@ -376,75 +252,7 @@
         </Table>
       </TabItem>
       <TabItem title="Stores">
-        <Table>
-          <TableHead>
-            <TableHeadCell class="!p-1" />
-            <TableHeadCell>Actions</TableHeadCell>
-            <TableHeadCell>Name</TableHeadCell>
-            <TableHeadCell>Address</TableHeadCell>
-            <TableHeadCell>Phone</TableHeadCell>
-          </TableHead>
-          <TableBody>
-            {#each itemsStores as item}
-              <TableBodyRow>
-                <TableBodyCell class="!p-1">
-                  <!-- <Checkbox /> -->
-                </TableBodyCell>
-                <TableBodyCell>
-                  <div class="flex justify-left">
-                    <Button on:click={() => (clickOutsideModal4 = true)}
-                      >Update</Button
-                    >
-                  </div>
-                  <Modal
-                    title="Update {item.name} Information"
-                    bind:open={clickOutsideModal4}
-                    autoclose
-                    outsideclose
-                    class="min-w-full"
-                  >
-                  <form on:submit={handleSubmit}>
-                    <div class="grid gap-4 mb-4 sm:grid-cols-2">
-                      <div>
-                        <Label for='price' class='mb-2'>Name</Label>
-                        <Input type='text' id='price' placeholder={item.name} />
-                      </div>
-                      <div>
-                        <Label for='price' class='mb-2'>Phone</Label>
-                        <Input type='text' id='price' placeholder={item.phone} />
-                      </div>
-                      <div class="sm:col-span-2">
-                        <Label for="address" class="mb-2">Address</Label>
-                        <Textarea id="address" placeholder="{item.phone}" rows="4" name="addresss"/>
-                      </div>
-                    </div>
-                    <div class="flex items-center space-x-4">
-                      <Button
-                        type="submit"
-                        class="w-fit"
-                        on:click={handleUpdate}
-                      >
-                        Update product
-                      </Button>
-                      <Button
-                        type="submit"
-                        class="w-fit"
-                        outline
-                        color="red"
-                        on:click={handleDelete}
-                      >
-                        Delete
-                      </Button>
-                    </div>
-                  </Modal>
-                </TableBodyCell>
-                <TableBodyCell>{item.name}</TableBodyCell>
-                <TableBodyCell>{item.address}</TableBodyCell>
-                <TableBodyCell>{item.phone}</TableBodyCell>
-              </TableBodyRow>
-            {/each}
-          </TableBody>
-        </Table>
+        <!-- Untuk Tabel Stores-->
       </TabItem>
     </Tabs>
   </Card>
